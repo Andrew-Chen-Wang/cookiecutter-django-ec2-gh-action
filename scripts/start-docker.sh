17380a440bffb935f568c7d83ec69eabe5d1e94b
Andrew-Chen-Wang <acwang02@gmail.com>
2020-04-17 14:50:13 -0400

    Fix travis
    Signed-off-by: Andrew-Chen-Wang <acwang02@gmail.com>

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -f local.yml build
-docker-compose -f local.yml run --rm django python manage.py migrate
-docker-compose -f local.yml up -d
+docker-compose -f production.yml build
+docker-compose -f production.yml run --rm django python manage.py migrate
+docker-compose -f production.yml up -d
